4e21ebd0fe0f7c81b36295de7c08e5cd95d5da39
Jeanne d'Arc Uwatowenimana <juwatow@gmail.com>
2024-11-23 21:58:56 +0000

    displaying user profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import {
+  Button,
+  Heading,
+  Flex,
+  View,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+import { Client } from "aws-amplify/data";
+import { Schema } from "../amplify/data/resource";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Configure Amplify
+Amplify.configure(outputs);
+
+const client: Client<Schema> = generateClient({
+  authMode: "userPool",
+});
+
+type UserProfile = {
+  id?: string;
+  email: string;
+};
+
+export default function App() {
+  const [userProfiles, setUserProfiles] = useState<UserProfile[]>([]);
+  const { signOut } = useAuthenticator((context) => [context.user]);
+
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+
+  async function fetchUserProfile() {
+    const { data: profiles } = await client.models.UserProfile.list();
+    setUserProfiles(profiles as UserProfile[]);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Alex would like to go to sleep
-        </p>
-      </div>
-      <p className="read-the-docs">
-        anuta is the best
-      </p>
-    </>
-  )
-}
+    <Flex
+      className="App"
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      width="70%"
+      margin="0 auto"
+    >
+      <Heading level={1}>My Profile</Heading>
 
-export default App
+      <Divider />
+
+      <Grid
+        margin="3rem 0"
+        autoFlow="column"
+        justifyContent="center"
+        gap="2rem"
+        alignContent="center"
+      >
+        {userProfiles.map((userProfile) => (
+          <Flex
+            key={userProfile.id || userProfile.email}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            gap="2rem"
+            border="1px solid #ccc"
+            padding="2rem"
+            borderRadius="5%"
+            className="box"
+          >
+            <View>
+              <Heading level={3}>{userProfile.email}</Heading>
+            </View>
+          </Flex>
+        ))}
+      </Grid>
+      <Button onClick={signOut}>Sign Out</Button>
+    </Flex>
+  );
+}
